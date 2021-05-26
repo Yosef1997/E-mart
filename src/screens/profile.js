cb89e3c89bb0dd4fa8aa86a6dad216eee3860820
unknown <yosefsitumorang97@gmail.com>
2021-05-26 16:18:38 +0700

    integration header

--- src/screens/profile.js
+++ src/screens/profile.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import Header from '../components/header';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import tailwind from 'tailwind-rn';
 
 const profile = () => {
   return (
-    <View>
-      <Text>Profile</Text>
+    <View style={tailwind('bg-white flex-1')}>
+      <Header style={false}>
+        <View style={tailwind('w-3/4')}>
+          <Text style={tailwind('font-bold text-2xl')}>Profile</Text>
+        </View>
+        <Ionicons name="mail" size={25} color="gray" style={tailwind('ml-2')} />
+        <Ionicons
+          name="ios-notifications"
+          size={25}
+          color="gray"
+          style={tailwind('ml-2')}
+        />
+      </Header>
     </View>
   );
 };
