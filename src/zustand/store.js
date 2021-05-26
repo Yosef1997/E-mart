d91bf0fc3df5473794b0a3c39fc028dd2ae16dc2
unknown <yosefsitumorang97@gmail.com>
2021-05-26 12:15:27 +0700

    create store

---
+++ src/zustand/store.js
@@ -0,0 +1,14 @@
+import create from 'zustand';
+import {devtools, persist} from 'zustand/middleware';
+
+let handleCart = set => ({
+  cart: {},
+  addCart: item => set(state => ({cart: {...state.cart, ...item}})),
+});
+
+handleCart = devtools(handleCart);
+handleCart = persist(handleCart);
+
+const useStore = create(handleCart);
+
+export default useStore;
