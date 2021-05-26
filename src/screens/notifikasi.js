516830f9c08c52ffc29a6d8d3cf09923d00977ac
unknown <yosefsitumorang97@gmail.com>
2021-05-26 18:08:41 +0700

    create screen order

---
+++ src/screens/notifikasi.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const notifikasi = () => {
+  return (
+    <View>
+      <Text>notifikasi</Text>
+    </View>
+  )
+}
+
+export default notifikasi
---
+++ src/screens/payment.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const payment = () => {
+  return (
+    <View>
+      <Text>Payment</Text>
+    </View>
+  )
+}
+
+export default payment
