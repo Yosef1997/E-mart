d97d779f90af7fa729d94c4fc9b14f7d25ae5212
unknown <yosefsitumorang97@gmail.com>
2021-05-26 12:38:05 +0700

    integration search input

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import Search from '../components/search';
 
 const home = () => {
   return (
     <View>
       <Text>Home</Text>
+      <Search />
     </View>
   );
 };
