0301257ff8d1fb45a3a166c5943485b5343be0a8
unknown <yosefsitumorang97@gmail.com>
2021-05-26 16:06:54 +0700

    integration header

--- src/screens/cart.js
+++ src/screens/cart.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import Header from '../components/header';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import tailwind from 'tailwind-rn';
 
 const cart = () => {
   return (
     <View>
-      <Text>Cart</Text>
+      <Header>
+        <Text style={tailwind('font-bold text-2xl')}>Cart</Text>
+        <Ionicons name="heart" size={25} color={'grey'} />
+      </Header>
     </View>
   );
 };
