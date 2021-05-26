621a2a4ebcb5963bbdf254650988918072b451e2
unknown <yosefsitumorang97@gmail.com>
2021-05-27 02:23:57 +0700

    create button

--- src/screens/detail.js
+++ src/screens/detail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, TouchableOpacity} from 'react-native';
+import {View, TouchableOpacity, Text} from 'react-native';
 import {useNavigation} from '@react-navigation/core';
 import tailwind from 'tailwind-rn';
 import Header from '../components/header';
@@ -7,11 +7,11 @@ import Search from '../components/search';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const detail = () => {
-  const navigation = useNavigation()
+  const navigation = useNavigation();
   return (
     <View style={tailwind('bg-white flex-1')}>
       <Header style={false}>
-        <TouchableOpacity onPress={()=>navigation.goBack('')}>
+        <TouchableOpacity onPress={() => navigation.goBack('')}>
           <Ionicons name="md-arrow-back" size={25} color={'grey'} />
         </TouchableOpacity>
         <View style={tailwind('w-4/6')}>
@@ -20,10 +20,18 @@ const detail = () => {
         <TouchableOpacity>
           <Ionicons name="share-social" size={25} color={'grey'} />
         </TouchableOpacity>
-        <TouchableOpacity onPress={()=>navigation.navigate('Cart')}>
+        <TouchableOpacity onPress={() => navigation.navigate('Cart')}>
           <Ionicons name="ios-cart" size={25} color={'grey'} />
         </TouchableOpacity>
       </Header>
+      <View style={tailwind('flex-row justify-between w-full absolute bottom-0 p-5')}>
+        <TouchableOpacity style={tailwind('bg-gray-50 border border-gray-200 rounded-3xl w-40 p-5')}>
+          <Text style={tailwind('text-center text-xl font-bold')}>Add to cart</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={()=>navigation.navigate('Delivery')} style={tailwind('bg-red-300 border border-red-300 rounded-3xl w-40 p-5')}>
+          <Text style={tailwind('text-center text-xl text-white font-bold')}>Buy now</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
